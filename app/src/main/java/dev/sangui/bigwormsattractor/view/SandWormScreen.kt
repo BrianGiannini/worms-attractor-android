17b73891c1886176048f5e1ce0fb51997951e909
Brian Giannini <brian.giannini.1@gmail.com>
2024-04-18 15:27:44 +0200

    fix animation coroutines being stucked

--- app/src/main/java/dev/sangui/bigwormsattractor/view/SandWormScreen.kt
+++ app/src/main/java/dev/sangui/bigwormsattractor/view/SandWormScreen.kt
@@ -1,5 +1,6 @@
 package dev.sangui.bigwormsattractor.view
 
+import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -8,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -18,9 +18,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,7 +30,7 @@ private val sandWormViewModel: SandWormViewModel by inject(SandWormViewModel::cl
 
 @Composable
 fun SandWormScreen() {
-    val isToggling = sandWormViewModel.toggleJob?.isActive == true
+    val isToggling by sandWormViewModel.isToggling.collectAsState()
     val toggleState by sandWormViewModel.toggleState.collectAsState()
 
     BoxWithConstraints(
@@ -120,8 +117,10 @@ fun SandWormScreen() {
             modifier = Modifier.align(Alignment.BottomEnd),
             onClick = {
                 if (isToggling) {
+                    Log.d("debug", "stop vibrator")
                     sandWormViewModel.stopPeriodicVibrator()
                 } else {
+                    Log.d("debug", "start vibrator")
                     sandWormViewModel.startPeriodicVibrator()
                 }
             },
--- app/src/main/java/dev/sangui/bigwormsattractor/view/SandWormViewModel.kt
+++ app/src/main/java/dev/sangui/bigwormsattractor/view/SandWormViewModel.kt
@@ -3,30 +3,44 @@ package dev.sangui.bigwormsattractor.view
 import androidx.lifecycle.ViewModel
 import dev.sangui.bigwormsattractor.logic.WormVibratorService
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class SandWormViewModel(private val wormVibrationService: WormVibratorService) : ViewModel() {
+
     private val _toggleState = MutableStateFlow(false)
     val toggleState = _toggleState.asStateFlow()
-    var toggleJob: Job? = null
-        private set
+
+    private var toggleJob: Job? = null
+    private val _isToggling = MutableStateFlow(false)
+    val isToggling = _isToggling.asStateFlow()
 
     fun startPeriodicVibrator() {
-        if (toggleJob?.isActive == true) return
+        // Prevent starting a new toggle if one is already active
+        if (_isToggling.value) return
 
         toggleJob = viewModelScope.launch {
-            while (isActive) {
-                _toggleState.value = !_toggleState.value
-                delay(1000L)
+            _isToggling.value = true
+            try {
+                while (true) {  // Continuously toggle the value
+                    _toggleState.value = !_toggleState.value
+                    delay(1000)  // Delay for 1 second
+                }
+            } finally {
+                _isToggling.value =
+                    false  // Ensure _isToggling is set to false when the coroutine is finished
             }
         }
     }
 
     fun stopPeriodicVibrator() {
-        toggleJob?.cancel()
-        toggleJob = null
+        if (toggleJob?.isActive == true) {
+            toggleJob?.cancel()
+            _isToggling.value = false  // Set toggling to false after stopping the job
+        }
     }
 
     fun triggerVibration() {
